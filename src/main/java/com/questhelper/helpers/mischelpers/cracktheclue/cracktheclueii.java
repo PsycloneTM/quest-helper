aeaf3e0ebb8d8a34d238c39f0fffded55d2d479e
CycloneTM <ianbuddy278@gmail.com>
2025-01-15 16:37:19 +0000

    Update cracktheclueii.java

--- src/main/java/com/questhelper/helpers/mischelpers/cracktheclue/cracktheclueii.java
+++ src/main/java/com/questhelper/helpers/mischelpers/cracktheclue/cracktheclueii.java
@@ -22,7 +22,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.questhelper.helpers.miniquests.cracktheclueii;
+package com.questhelper.helpers.mischelpers.cracktheclueii;
 
 import com.questhelper.panel.PanelDetails;
 import com.questhelper.questhelpers.BasicQuestHelper;
